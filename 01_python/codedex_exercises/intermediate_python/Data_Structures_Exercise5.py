1e0dcea671abfdc2dcddffd7626d47e69163c817
Dennis Paus <dennis.paus.2002@gmail.com>
2026-04-23 10:20:08 +0200

    codedex datastructures exercises 5 Variante 2 List Comprehension

--- 01_python/codedex_exercises/intermediate_python/Data_Structures_Exercise5.py
+++ 01_python/codedex_exercises/intermediate_python/Data_Structures_Exercise5.py
@@ -14,7 +14,7 @@ Let's start analyzing!
     Calculate the average statistics (e.g., yards gained, touchdowns) for all players and print the results.
 
 """
-
+# Ursprüngliche Version
 tyreek = {'name': 'Tyreek Hill', 'position': 'Wide Receiver', 'jersey_number': 10, 'yards_gained': 150, 'touchdowns': 2}
 jefferson = {'name': 'Justin Jefferson', 'position': 'Wide Receiver', 'jersey_number': 18, 'yards_gained': 137, 'touchdowns': 1}
 mahomes = {'name': 'Patrick Mahomes', 'position': 'Quarterback', 'jersey_number': 15, 'yards_gained': 312, 'touchdowns': 3}
@@ -31,3 +31,36 @@ print(mahomes["touchdowns"])
 
 avg_touchdowns = (tyreek["touchdowns"] + jefferson["touchdowns"] + mahomes["touchdowns"] + henry["touchdowns"])/4
 print(avg_touchdowns)
+
+# Version 2
+players = [
+    {'name': 'Tyreek Hill', 'position': 'Wide Receiver', 'jersey_number': 10, 'yards_gained': 150, 'touchdowns': 2},
+    {'name': 'Justin Jefferson', 'position': 'Wide Receiver', 'jersey_number': 18, 'yards_gained': 137, 'touchdowns': 1},
+    {'name': 'Patrick Mahomes', 'position': 'Quarterback', 'jersey_number': 15, 'yards_gained': 312, 'touchdowns': 3},
+    {'name': 'Derrick Henry', 'position': 'Running Back', 'jersey_number': 22, 'yards_gained': 189, 'touchdowns': 2}
+]
+
+"""
+Leitfrage List Comprehension: 
+„Kann man den Code noch in einem Satz auf Deutsch beschreiben?"
+Wenn ja → List Comprehension. Wenn nein → normale Schleife.
+"""
+
+names = [player["name"] for player in players] # für jeden Spieler in der Spielerliste trage bitte den Namen in eine neue Liste ein
+print("Alle registrierten Spieler: ",names)
+
+positions = [player["position"] for player in players]
+print("Die jeweiligen Positionen: ",positions)
+
+players[1]["touchdowns"] += 3
+players[1]["yards_gained"] += 50
+print("Angepasste Touchdowns und Yards bei: ", players[1]["name"], players[1]["touchdowns"], players[1]["yards_gained"]) 
+
+all_yards = sum(player["yards_gained"] for player in players)
+avg_yard = all_yards / len(players)
+print("Durchschnittliche Yards", avg_yard)
+
+all_touchdowns = sum(player["touchdowns"] for player in players)
+avg_td = all_touchdowns / len(players)
+print("Durchschnittliche Touchdowns: ", avg_td)
+
